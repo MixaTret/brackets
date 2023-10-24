056352285832ad0a42a924ed821721bc77789476
MixaTret <mikhail.tretsiakevich@gmail.com>
2023-10-24 15:48:47 +0100

    feat: all tests were done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const configMap = new Map(bracketsConfig.map(([open, close]) => [close, open]));
+  const sameBrackets = new Set(bracketsConfig.map(([open, close]) => open === close ? open : ''));
+
+  for (const char of str) {
+    if (sameBrackets.has(char)) {
+      if (stack.length > 0 && stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (configMap.has(char)) {
+      const lastOpenBracket = stack.pop();
+      if (lastOpenBracket !== configMap.get(char)) {
+        return false;
+      }
+    } else {
+      stack.push(char);
+    }
+  }
+  return stack.length === 0;
 }
